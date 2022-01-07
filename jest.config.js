18cd9086155d31bfc0ec7bcd84b7218aa3fcf77b
Aleksey Kozin <aleksey.kozin@zoftify.com>
2022-01-08 01:45:26 +0300

    feat: ui

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-native-picker|@hookform|@react-native-cookies/cookies)',
   ],
+  passWithNoTests: true,
   moduleNameMapper: {
     '^.+.(css|svg|styl|less|sass|scss|jpg|ttf|woff|woff2|mp3)$':
       'jest-transform-stub',
